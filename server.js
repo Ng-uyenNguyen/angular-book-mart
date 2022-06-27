605d27e2020c8f3857a9447f271255bea94b1460
Ng-uyen Nguyen <nguyenndb511@gmail.com>
2022-06-27 22:46:03 +0700

    config package.json

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
 const app = express();
- 
+
 const forceSSL = function () {
   return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(["https://", req.get("Host"), req.url].join(""));
     }
     next();
-  }
+  };
 };
-app.use(express.static('./dist/book-mart'));
- 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/book-mart/index.html'));
+app.use(express.static("./dist/book-mart"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/dist/book-mart/index.html"));
 });
- 
+
 app.use(forceSSL());
- 
+
 app.listen(process.env.PORT || 8080);
