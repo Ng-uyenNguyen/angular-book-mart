0b97e2f768fdc46cb2046d35224f93c88d04b9f0
Ng-uyen Nguyen <nguyenndb511@gmail.com>
2022-06-28 00:11:35 +0700

    config package.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/angular-heroku-demo"));
+app.use(express.static(__dirname + "/dist/angular-bookmart-app"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/angular-heroku-demo/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/angular-bookmart-app/index.html"));
 });
 app.listen(process.env.PORT || 8080);
